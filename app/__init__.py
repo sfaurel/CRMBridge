2e5d5ed8052c1b9512f45b9eed3d3be6596dbeae
sfaure <sebastian.faure.l@gmail.com>
2024-11-21 13:28:02 -0300

    rafactor to factory pattern

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,10 @@
-from flask import Blueprint
-from flask_restx import Api
+from flask import Flask
+from app.utils.conf import load_config
 
-from .controllers.status_controller import status_ns
-from .controllers.leads_controller import leads_ns
 
-blueprint = Blueprint("CRM api", __name__, url_prefix="/api/")
+def create_app():
+    app = Flask(__name__)
 
-api = Api(
-    blueprint,
-    version="1.0",
-    title="CRM Api",
-    description="API to integrate with a CRM",
-)
+    load_config(app)
 
-api.add_namespace(status_ns, path='/status')
-api.add_namespace(leads_ns, path='/leads')
+    return app
--- run.py
+++ run.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Blueprint
+from flask_restx import Api
 
-from app import blueprint
-from app.utils.conf import load_config
+from app.controllers.status_controller import status_ns
+from app.controllers.leads_controller import leads_ns
+from app import create_app
 
-app = Flask(__name__)
-load_config(app)
+blueprint = Blueprint("CRM api", __name__, url_prefix="/api/")
 
+api = Api(
+    blueprint,
+    version="1.0",
+    title="CRM Api",
+    description="API to integrate with a CRM",
+)
+
+api.add_namespace(status_ns, path='/status')
+api.add_namespace(leads_ns, path='/leads')
+
+app = create_app()
 app.register_blueprint(blueprint)
+
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
